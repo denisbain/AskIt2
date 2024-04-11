40cc51d6e9bbd1d05761b7602b56476307918dd9
Denis Bain <baindenis95@gmail.com>
2024-04-11 06:24:35 -0300

    Форма для создания связанных записей

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+    resources :answers, only: %i[create]
+  end
   #resources :questions, only: %i[index new edit create update destroy show]
 
   root 'pages#index'
