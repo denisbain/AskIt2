58ab15561f539e1353fd753119c1e81dfb32bdbb
Denis Bain <baindenis95@gmail.com>
2024-04-11 06:42:34 -0300

    Валидация для answers  и обработка ошибок в форме

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,8 @@
 class AnswersController < ApplicationController
   before_action :set_question!
   def create
-    answer = @question.answers.build answer_params
-    if answer.save
+    @answer = @question.answers.build answer_params
+    if @answer.save
       flash[:success] = 'Answer created!'
       redirect_to question_path(@question)
     else
