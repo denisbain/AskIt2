34992915669496452b2f78280d7368cc21d9cf64
Denis Bain <baindenis95@gmail.com>
2024-04-11 05:47:42 -0300

    Запись в журнал событий об ошибке notfound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :notfound
 
   private
-  def notfound
+  def notfound(exception)
+    logger.warn exception
     render file: 'public/404.html', status: :not_found, layout: false
   end
 end
