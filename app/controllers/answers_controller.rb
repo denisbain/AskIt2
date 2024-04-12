54943c17303f9b3609ad2b4fa9fabc0c2116570b
Denis Bain <baindenis95@gmail.com>
2024-04-12 06:50:45 -0300

    Исправление: корректный вывод ответов для вопроса #2

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -24,7 +24,7 @@ class AnswersController < ApplicationController
       flash[:success] = 'Answer created!'
       redirect_to question_path(@question)
     else
-      @answers = Answer.order created_at: :desc
+      @answers = @question.answers.order created_at: :desc
       render 'questions/show'
     end
   end
