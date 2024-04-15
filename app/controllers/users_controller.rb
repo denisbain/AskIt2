06327f2180858b993cf91e786ba6d8145cde9ff9
Denis Bain <baindenis95@gmail.com>
2024-04-15 17:38:30 -0300

    Add 'Sigh Up' on the nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = "Weclome to the app, #{@user.name}"
+      flash[:success] = "Weclome to the app, #{@user.name}!"
       redirect_to root_path
     else
       render :new
