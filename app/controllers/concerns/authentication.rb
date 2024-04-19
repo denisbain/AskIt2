57f8f78a8e63e9602ad3fb80feedeaed64e53f4e
Denis Bain <baindenis95@gmail.com>
2024-04-19 06:32:57 -0300

    check, the user is logged in or not

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -11,6 +11,17 @@ module Authentication
       current_user.present?
     end
 
+    def require_no_authentication
+      return if !user_signed_in?
+      flash[:warning] = 'You are already signed in'
+      redirect_to root_path
+    end
+
+    def require_authentication
+      return if user_signed_in?
+      flash[:warning] = 'You are not signed in'
+      redirect_to root_path
+    end
     def sign_out
       session.delete :user_id
     end
