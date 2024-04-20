2db836b30e59e2935d407dd779c60077549e079a
Denis Bain <baindenis95@gmail.com>
2024-04-20 10:58:29 -0300

    Check old password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :old_password)
   end
 end
