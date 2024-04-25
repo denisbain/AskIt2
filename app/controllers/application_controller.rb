227d3cd4b6c5385bef0f13e97df103c652afdfa7
Denis Bain <baindenis95@gmail.com>
2024-04-25 16:46:01 -0300

    Switch localization #2. Assigning locale from URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,15 @@ class ApplicationController < ActionController::Base
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale locale, &action
   end
+
+  def locale_from_url
+    locale = params[:locale]
+
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+    nill
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
