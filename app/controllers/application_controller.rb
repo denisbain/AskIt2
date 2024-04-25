69cc2cc8bf4f93feb1145f3ff10dc367984cebcc
Denis Bain <baindenis95@gmail.com>
2024-04-25 16:01:25 -0300

    Switch localization #1. Change localization through URL request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include ErrorHandling
   include Authentication
+
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
 end
