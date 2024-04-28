67b0981cfe9f34124d1f468c7f2c8c2b7765b911
Denis Bain <baindenis95@gmail.com>
2024-04-28 11:34:08 -0300

    Linking questions and answers to users

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,7 +9,7 @@ class AnswersController < ApplicationController
   def edit; end
 
   def create
-    @answer = @question.answers.build(answer_params)
+    @answer = @question.answers.build(answer_create_params)
     if @answer.save
       flash[:success] = 'Answer created!'
       redirect_to question_path(@question)
@@ -22,7 +22,7 @@ class AnswersController < ApplicationController
   end
 
   def update
-    if @answer.update answer_params
+    if @answer.update answer_update_params
       flash[:success] = 'Answer updated!'
       redirect_to question_path(@question, anchor: dom_id(@answer))
     else
@@ -38,10 +38,13 @@ class AnswersController < ApplicationController
 
   private
 
-  def answer_params
-    params.require(:answer).permit(:body)
+  def answer_create_params
+    params.require(:answer).permit(:body).merge(user_id: current_user.id)
   end
 
+  def answer_update_params
+    params.require(:answer).permit(:body)
+  end
   def set_question!
     @question = Question.find(params[:question_id])
   end
