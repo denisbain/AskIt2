e1acd61e4482a6dad214ecdee9012e2dc71aafeb
Denis Bain <baindenis95@gmail.com>
2024-05-03 11:00:59 -0300

    Comments #4. Check for which object created comment, delete comment

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,4 +2,9 @@
 class CommentDecorator < ApplicationDecorator
   delegate_all
   decorates_association :user
+
+  def for?(commentable)
+    commentable = commentable.object if commentable.decorated?
+    commentable == self.commentable
+  end
 end
