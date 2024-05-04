0024427150cd3ddff34f8bc567016da34d198746
Denis Bain <baindenis95@gmail.com>
2024-05-04 08:02:17 -0300

    Add concern for comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  concern :commentable do
+    resources :comments, only: %i[create destroy]
+  end
+
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
     resource :session, only: %i[new create destroy]
 
     resources :users, only: %i[new create edit update]
 
-    resources :questions do
-      resources :comments, only: %i[create destroy]
+    resources :questions, concern: :commentable do
       resources :answers, expect: %i[new show]
     end
 
-    resources :answers, expect: %i[new show] do
-      resources :comments, only: %i[create destroy]
-    end
+    resources :answers, expect: %i[new show], concern: :commentable
 
     namespace :admin do
       resources :users, only: %i[index create]
