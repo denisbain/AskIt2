068e54b18feee2858f226e624b34c1c326c6d330
Denis Bain <baindenis95@gmail.com>
2024-05-16 09:32:43 -0300

    Add tags for question #5. Add i18n for select 2

--- app/javascript/scripts/select2.js
+++ app/javascript/scripts/select2.js
@@ -1,6 +1,13 @@
 import $ from 'jquery'
 import 'select2/dist/js/select2'
-import 'select2/dist/css/select2.min.css';
+import 'select2/dist/css/select2.min.css'
+import * as Select2Ru from 'select2/src/js/select2/i18n/ru'
+import * as Select2En from 'select2/src/js/select2/i18n/en'
+
+const select2_langs = {
+    ru: Select2Ru,
+    en: Select2En
+}
 $(document).on("turbolinks:load", function() {
     $('.js-multiple-select').each(function () {
         const $this = $(this);
@@ -9,7 +16,8 @@ $(document).on("turbolinks:load", function() {
             theme: 'bootstrap-5',
             width: $this.data("width") ? $this.data("width") : $this.hasClass("w-100") ? "100%" : "style",
             placeholder: $this.data("placeholder"),
-            allowClear: Boolean($this.data("allow-clear"))
+            allowClear: Boolean($this.data("allow-clear")),
+            language: select2_langs[$('body').data('lang')]
         };
 
         $this.select2(opts);
