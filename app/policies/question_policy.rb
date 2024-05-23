90ad3dad9874d1fa359ac9b95df01ac9801c44bc
Denis Bain <baindenis95@gmail.com>
2024-05-23 10:02:27 -0300

    Pundit #5. policies destroy questions

--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -6,7 +6,7 @@ class QuestionPolicy < ApplicationPolicy
     user.admin_role? || user.moderator_role? || user.author?(record)
   end
   def destroy?
-    
+    user.admin_role? || user.author?(record)
   end
   def index?
     true
