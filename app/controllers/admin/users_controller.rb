ed5f7eb19593ed66e809d5dd6792d02f1b7a4a61
Denis Bain <baindenis95@gmail.com>
2024-05-27 10:21:35 -0300

    policies for admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,9 @@
 module Admin
-  class UsersController < ApplicationController
+  class UsersController < BaseController
     before_action :require_authentication
     before_action :set_user!, only: %i[edit update destroy]
+    before_action :authorize_user!
+    after_action :verify_authorized
 
     def index
       respond_to do |format|
@@ -62,6 +64,11 @@ module Admin
 
     def user_params
       params.require(:user).permit(:email, :name, :password, :password_confirmation, :role).merge(admin_edit: true)
+    end
+
+    def authorize_user!
+      current_user
+      authorize(@user || User)
     end
   end
 end
