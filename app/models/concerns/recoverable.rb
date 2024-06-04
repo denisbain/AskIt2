5d0a3e5d7270e02817fcc210ac54b56a01a5e9c7
Denis Bain <baindenis95@gmail.com>
2024-06-04 11:47:08 -0300

    Reset password #6. Validity of token (lifetime)

--- app/models/concerns/recoverable.rb
+++ app/models/concerns/recoverable.rb
@@ -6,5 +6,8 @@ module Recoverable
       self.update password_reset_token: digest(SecureRandom.urlsafe_base64),
                   password_reset_token_sent_at: Time.current
     end
+    def password_reset_period_valid?
+      password_reset_token_sent_at.present? && Time.current - password_reset_token_sent_at <= 60.minutes
+    end
   end
 end
