e9855c0de8562e82a65c5e01bbb3f7c8451fd54b
Denis Bain <baindenis95@gmail.com>
2024-06-04 09:38:32 -0300

    Reset password #3. Finish method create for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,13 @@ class PasswordResetsController < ApplicationController
   before_action :require_no_authentication
 
   def create
-    PasswordResetMailer.with(user: User.first).reset_email.deliver_later
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      @user.set_password_reset_token
+      PasswordResetMailer.with(user: User.first).reset_email.deliver_later
+    end
+
+    flash[:success] = t ".success"
+    redirect_to new_session_path
   end
 end
