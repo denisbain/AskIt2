57d891196a49b9e38cbfdeb8aff327de7392574f
Denis Bain <baindenis95@gmail.com>
2024-06-04 11:40:30 -0300

    Reset password #5. Search user by reset token

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
   before_action :require_no_authentication
+  before_action :set_user, only: %i[edit update]
 
   def create
     @user = User.find_by(email: params[:email])
@@ -11,5 +12,16 @@ class PasswordResetsController < ApplicationController
     flash[:success] = t ".success"
     redirect_to new_session_path
   end
-  
+
+  def edit
+  end
+
+  private
+  def set_user
+    @user = User.find_by email: params[:user][:email],
+                         password_reset_token: params[:user][:password_reset_token]
+    
+    redirect_to(new_session_path, flash: {warning: t('password_resets.fail')}) unless @user&.password_reset_period_valid?
+
+  end
 end
